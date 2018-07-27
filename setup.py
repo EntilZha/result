9d65544f5fbfc4bf496d613983ef8b2e398b3cd4
Pedro Rodriguez <ski.rodriguez@gmail.com>
2018-07-26 20:54:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='result',
     description='',
-    long_description=long_description,
+    long_description='',
     url='https://github.com/EntilZha/result',
     author='Pedro Rodriguez',
     author_email='ski.rodriguez@gmail.com',
